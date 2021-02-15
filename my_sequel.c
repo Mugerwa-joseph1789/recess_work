f2c1a20594d040e39d65c57d2d3d060f075ba02f
yusuf-11 <74214203+yusuf-11@users.noreply.github.com>
2021-02-15 16:47:27 +0300

    Add files via upload

---
+++ my_sequel.c
@@ -0,0 +1,60 @@
+#include <mysql.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct patient{
+    char patient_fname[50];
+    char patient_sname[50];
+    char DOI[50];
+    char patient_gender[50];
+    char status[50];
+    char User_name[30];
+};
+
+
+
+int main(){
+	MYSQL *conn = mysql_init(NULL);
+	
+	char sql[1024];
+	FILE *file;
+	float tc = clock();
+	int flag = 0;
+
+ struct patient details[100];
+
+	if (conn == NULL){
+		/* code */
+		fprintf(stderr,"%s\n",mysql_error(conn));
+		exit(1);
+	}
+	
+	if (mysql_real_connect(conn,"127.0.0.1","root","","Covid_cases",0,NULL,0) == NULL){
+		fprintf(stderr,"%s\n",mysql_error(conn));
+		exit(1);
+	}
+
+	 file = fopen("sick_file.txt","r");
+    if(file == NULL){
+        perror("Error in file:");
+        exit(1);
+    }else{
+         while(!feof(file)){
+               fscanf(file,"%s %s %s %s %s %s \n",details[flag].patient_fname,details[flag].patient_sname,details[flag].patient_gender,details[flag].date,details[flag].status,details[flag].User_name);
+
+				sprintf(sql,"INSERT INTO `sick_file`( id,patient_fname,patient_sname,dte,patient_gender,status,User_name)VALUES('0','%s','%s','%s','%s','%s','%s');",details[flag].patient_fname,details[flag].patient_sname,details[flag].date,details[flag].patient_gender,details[flag].status,details[flag].User_name);
+			 
+                flag++;
+        } 
+		
+       // puts(details[2].patient_sname);
+        fclose(file);
+    }
+	if (mysql_query(conn,sql)){
+				fprintf(stderr,"%s\n",mysql_error(conn));
+				exit(1);
+			 }
+		printf("The client id for mysql :%s  has inserted data successfully  in %f seconds\n",mysql_get_client_info(),tc/CLOCKS_PER_SEC);
+		mysql_close(conn);
+		exit(1);
